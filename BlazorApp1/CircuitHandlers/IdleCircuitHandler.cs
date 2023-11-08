623b572a0e45834d7c119fc57ed773568123f9ce
Thomas McQueen <thomas.mcqueen@exasoftgroup.com>
2023-11-08 09:25:35 +0000

    Add project files.

---
+++ BlazorApp1/CircuitHandlers/IdleCircuitHandler.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.Extensions.Options;
+
+using System.Runtime.CompilerServices;
+
+namespace BlazorWebAppExperiment.CircuitHandlers;
+
+public sealed class IdleCircuitHandler : CircuitHandler, IDisposable
+{
+    readonly System.Timers.Timer timer;
+    readonly ILogger logger;
+
+    public IdleCircuitHandler(IOptions<IdleCircuitOptions> options,
+        ILogger<IdleCircuitHandler> logger)
+    {
+        timer = new System.Timers.Timer();
+        timer.Interval = options.Value.IdleTimeout.TotalMilliseconds;
+        timer.AutoReset = false;
+        timer.Elapsed += CircuitIdle;
+        this.logger = logger;
+    }
+
+    private void CircuitIdle(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        logger.LogInformation("{name} is idle", nameof(CircuitIdle));
+    }
+
+
+
+    public override Func<CircuitInboundActivityContext, Task> CreateInboundActivityHandler(
+        Func<CircuitInboundActivityContext, Task> next)
+    {
+        return context =>
+        {
+            logger.LogInformation("{name} is activated", nameof(CreateInboundActivityHandler));
+            timer.Stop();
+            timer.Start();
+            return next(context);
+        };
+    }
+
+    public void Dispose()
+    {
+        timer.Dispose();
+    }
+}
+
+public class IdleCircuitOptions
+{
+    public TimeSpan IdleTimeout { get; set; } = TimeSpan.FromSeconds(5);
+}
+
+public static class IdleCircuitHandlerServiceCollectionExtensions
+{
+    public static IServiceCollection AddIdleCircuitHandler(
+        this IServiceCollection services,
+        Action<IdleCircuitOptions> configureOptions)
+    {
+        services.Configure(configureOptions);
+        services.AddIdleCircuitHandler();
+        return services;
+    }
+
+    public static IServiceCollection AddIdleCircuitHandler(
+        this IServiceCollection services)
+    {
+        services.AddScoped<CircuitHandler, IdleCircuitHandler>();
+        return services;
+    }
+}
